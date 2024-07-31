7433a21bbb92d455ac5df41de5bcbde1243a3160
Gary Wei <hao-jen.wei@houzz.com>
2024-07-31 18:17:12 +0800

    integrate google sheet API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import './App.css';
+import React, { useCallback, useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import ListGroup from 'react-bootstrap/ListGroup';
-import React from 'react';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { JWT } from 'google-auth-library';
+
+import './App.css';
+
+const serviceAccountAuth = new JWT({
+  email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  key: process.env.GOOGLE_PRIVATE_KEY.split(String.raw`\n`).join('\n'),
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
 
 const App = props => {
+  const [name, setName] = useState('');
+
+  const submit = async (name, guess) => {
+    const doc = new GoogleSpreadsheet(process.env.SHEET_ID, serviceAccountAuth);
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+    await sheet.addRow({ Name: name, Guess: guess });
+  }
+
+  const onSubmitClick = useCallback(() => {
+    const selectedValue = document.querySelector('input[name="genders"]:checked')  
+    const guess = selectedValue?.value;
+    submit(name, guess);
+  }, [name]);
+  
   return (
     <div class='container-fluid text-center' id="container">
       <div class='p-3'>
@@ -25,20 +49,20 @@ const App = props => {
           <h2>選出你的預測吧</h2>
           <div class="d-flex justify-content-center" id="gender-radios">
             <div>
-              <input type="radio" class="btn-check" name="options" id="girl" autocomplete="off" />
+              <input type="radio" class="btn-check" name="genders" id="girl" value="Phoebe" autocomplete="off" />
               <label class="btn rounded-circle gender-label" for="girl">Phoebe <br/>女生</label>
             </div>
             <div>
-              <input type="radio" class="btn-check" name="options" id="boy" autocomplete="off"/>
+              <input type="radio" class="btn-check" name="genders" id="boy" value="Jensen" autocomplete="off"/>
               <label class="btn rounded-circle gender-label" for="boy">Jensen <br/>男生</label>
             </div>
           </div>
         </Form.Group>
         <Form.Group controlId='genderForm.Name' class='mt-3'>
           <Form.Label>你是誰？</Form.Label>
-          <Form.Control type="text" placeholder="浩仁" />
+          <Form.Control type="text" placeholder="浩仁" onChange={e => setName(e.target.value)} value={name}  />
         </Form.Group>
-        <Button type="submit" size='lg' className="mt-4" id="submitBtn" variant='outline-secondary'>看結果！</Button>
+        <Button size='lg' className="mt-4" id="submitBtn" variant='outline-secondary' onClick={onSubmitClick}>看結果！</Button>
       </Form>
       
       
